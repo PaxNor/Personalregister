2c85c26ca5e63a7ef3d0d90ccfc17a171edd583e
Lexicon Student 1 <Lexicon Student 1@DESKTOP-OF8V11H>
2022-05-10 16:59:55 +0200

    Add project files.

---
+++ Personalregister/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Employee
+{
+	string name;
+	int salary;
+
+	public Employee()
+	{
+		this.name = "undefined";
+	}
+
+	public Employee(string name, int salary)
+	{
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public void setName(string name) { this.name = name; }
+	public void setSalary(int salary) { this.salary = salary; }
+	public string getName() { return this.name; }
+	public int getSalary() { return this.salary; }
+}
---
+++ Personalregister/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Employee[] staff;
+
+while(true)
+{
+    Console.Write("Add employee 1. Print employees 2. ");
+    string choice = Console.ReadLine();
+
+    if(choice == "1")
+    {
+        Console.Write("Enter name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter salary: ");
+        string s = Console.ReadLine();
+        int salary = Int32.Parse(s);
+
+        Employee e = new Employee(name, salary);
+
+    }
+
+    if(choice == "2")
+    {
+        // hann inte
+    }
+}
